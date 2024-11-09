6a2fed233ca237c5a71fb565b38e8152d571238a
cheesy d <cheesyd64@hotmail.com>
2024-11-08 22:52:19 -0800

    Completed basic adding subtitle file from transcription

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import whisper
 import tiktoken
 import subprocess
+import pysubs2
 
 app = Flask(__name__)
 
@@ -24,6 +25,23 @@ subprocess.run(ffmpegExtract, check=True)
 #transcribe audio file
 result = model.transcribe(audioPath)
 print(result["text"])
+
+#create subtitle file
+subs = pysubs2.SSAFile()
+
+for subLine in result["segments"]:
+    #get start and end time of subtitle line
+    start_time = subLine["start"]
+    end_time = subLine["end"]
+    text = subLine["text"]
+
+    #format and add subtitle line to file
+    line = pysubs2.SSAEvent(start=start_time, end=end_time, text=text) 
+    subs.events.append(line)   
+    print(f"{start_time} --> {end_time} {text}")
+
+#save subtitle file
+subs.save("files/subtitles.srt", format_='srt')
 
 #testing translating
 #resultSpanish = model.transcribe(audioPath, task='translate', language='Spanish') 
